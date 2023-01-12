dfde3534fb8e9d125b4e96e6690810b9014be5f9
bdemeshev <boris.demeshev@gmail.com>
2023-01-12 18:32:44 +0300

    gogogo

---
+++ class_01.R
@@ -0,0 +1,14 @@
+# let's start
+
+# 2 parts: me + Dean Fantazzini
+# my part: intro to R, 4 weeks
+# datacamp 
+# two mini-courses on datacamp
+
+# web page of the course
+# https://github.com/bdemeshev/icef_r4finance_2023/
+  
+# tg chat
+# https://t.me/+M-2EsPzrZlYzYWVi
+
+
