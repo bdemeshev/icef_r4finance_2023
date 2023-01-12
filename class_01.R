98670d32bba1c09d24a8982d3d4469fc97c21004
bdemeshev <boris.demeshev@gmail.com>
2023-01-12 18:49:07 +0300

    gogogdo

--- class_01.R
+++ class_01.R
@@ -11,4 +11,26 @@
 # tg chat
 # https://t.me/+M-2EsPzrZlYzYWVi
 
+# packages! :)
+# 1. how to install packages (once)
+# 2. how to attach packages (every time you use it)
+
+# tidyverse, rio, estimatr, skimr
+
+# windows:
+# compile packages from source?
+# NO
+
+library(tidyverse) # data manipulation, plots, ...
+library(rio) # read many-many file formats
+library(skimr) # descriptive statistics
+library(estimatr) # robust inference in regression
+
+
+
+
+
+
+
+
 
