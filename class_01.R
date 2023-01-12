79c7f8de6f237af8981f3577dd2b833fa3bee24f
bdemeshev <boris.demeshev@gmail.com>
2023-01-12 19:09:58 +0300

    gogogdo

--- class_01.R
+++ class_01.R
@@ -26,6 +26,36 @@ library(rio) # read many-many file formats
 library(skimr) # descriptive statistics
 library(estimatr) # robust inference in regression
 
+# ctrl + enter or cmd + enter to execute a line
+
+# install.packages('rio')
+
+
+a = 5 # my current style
+b <- 7 # also a valid assignment style 
+9 -> d # also a valid assignment style 
+
+# learn to use TAB key!!!
+
+library(estimatr) # libr <tab> esti <down> <tab>
+
+my_lovely_variable = 8
+my_lovely_variable # my_ <tab>
+
+# minimal style guide
+# 1. Space after comma
+# 2. Space around math operators +, -, *, /
+
+x=5+b+d # bad style
+x = 5 + b + d # good style
+
+# start from built-in dataset
+# modify it
+# save it 
+# read it
+# plots
+# regression with robust se
+
 
 
 
